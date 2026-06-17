9efe24fb7f2993bb141819953a0e7e7b355384b0
ustaxs <abbassaudat200@gmail.com>
2026-06-17 12:52:18 +0100

    Add Idea Creation Form with Multi-Step Validation

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,23 +1,31 @@
-import "./globals.css"
+import "./globals.css";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/toaster";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Hello-World | Crypto Idea-Sharing Platform",
-  description: "A decentralized, community-driven platform for crypto ideas sharing and insights built on Stellar",
-    generator: 'Divineifed1'
-}
+  description:
+    "A decentralized, community-driven platform for crypto ideas sharing and insights built on Stellar",
+  generator: "Divineifed1",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
